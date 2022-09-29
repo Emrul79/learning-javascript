3fbc69b29da183924ac2aba2371d743d93a83b24
Emrul Hassan Biplob <hemrul494@gmail.com>
2022-09-29 18:44:02 +0600

    lesson-1

---
+++ lesson-1/index.js
@@ -0,0 +1,35 @@
+// document.getElementById() //console.log();
+//In the above sentance the ** getElementById() ** and ** log()** both are method(functions) of document and console object{}!!
+//whenever we call those method they take an argument or deta to work with.
+// it works like, the methode is cheap or cooker. the argument you give is like ingredients.
+//then it will return you delicioue resipie otherwise it will return nothing!
+
+//  DOM = Document - Object - Model. //when someone talk about DOM ,don't be sceard .just  translated to how to use javascript to modify your website.
+
+//we will learn this 3 thing separately.
+//1.Document: what we will interect with is a HTML document.
+//2.Object: it just take the HTML document and show the JavaScript Object.
+//  Document = { Object }.
+//Model: Model is just a represntation or mobelation. its just the represntation of javascript of your html document.
+//for example:
+//Real Thing: <h1 id='myCounter'>this is a simple header</h1> ->
+//Your JS Model : let myCounter= document.getElementById('myCounter');
+
+//String vs Number: In a resling game between string & number-Number always win.
+//for example:
+// let mynumber = 65;
+//let mystring= 'hi my name is biplob & i am ' + mynumber + 'years old'.
+//so we can say that if a number added with a string this number will be convert into a string.
+
+/*
+(NUMBER VS STRING)
+
+console.log(5 + '7');//output=57 | because number vs string = winner (string);
+console.log('5' + "7");//output=57
+console.log(5 + +"7");//output=12 | when we put a plus sing befoere a string , it will convert intu a number!!
+
+console.log('5' + 7);//output=57
+
+*/
+
+/*Counter App End here */
