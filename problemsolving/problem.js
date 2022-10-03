7a8168069cea812493eb1b70b3e49b40ad52a47f
Emrul Hassan <hemrul494@gmail.com>
2022-10-03 23:42:33 +0600

    solving-problem-is-fun

--- problemsolving/problem.js
+++ problemsolving/problem.js
@@ -21,5 +21,67 @@ function converter(array) {
   });
   return elements;
 }
-console.log(converter(messege));
+
 //problem-1:solved!!!
+
+//problem:2-given a sentence 's' and integer 'k' .return the first 'k' works of sentence s.
+
+//solution:1=
+
+function sliceArray(string, stop) {
+  let newString = string.split(" ");
+  newString.length = stop;
+  return newString.join(" ");
+}
+
+//solution:2
+function mySlice(string, stop) {
+  let splitString = string.split(" ");
+  let newSAtring = [];
+  for (let i = 0; i < stop; i++) {
+    newSAtring.push(splitString[i]);
+  }
+  return newSAtring.join(" ");
+}
+
+// solution:3
+
+function slc(s, k) {
+  let count = 0;
+  for (var i = 0; i < s.length; i++) {
+    s[i] === " " && count++;
+    console.log(count);
+    if (count === k) {
+      s = s.slice(0, i);
+      break;
+    }
+  }
+  return s;
+}
+
+//problem:3
+//given an array of integers "nums" .return an array that contanis the running sums of element!
+
+// solution:
+function sumofPreviousArray(array) {
+  for (var i = 1; i < array.length; i++) {
+    array[i] += array[i - 1];
+  }
+  return array;
+}
+
+//problem:4
+//given an integer number.check weather the number is palindrome or not!
+
+function ispalindrome(number){
+  let reminder;
+let reverse = 0;
+let orginal = number;
+while (orginal > 0) {
+  reminder = orginal % 10;
+  reverse= reverse * 10 + reminder;
+  orginal= Math.floor(orginal/10)
+}
+}
+ispalindrome(121)
+
