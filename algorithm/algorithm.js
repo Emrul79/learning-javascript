b446f7436ef866c6d6071cd72cd13b60d8fde79b
Emrul Hassan <hemrul494@gmail.com>
2022-10-04 20:51:42 +0600

    introduction to algorithm

---
+++ algorithm/algorithm.js
@@ -0,0 +1,32 @@
+/* what is algorithm?
+
+An algorithm is a set of well-defined instructions to solve a particular problem.
+(Ingredients) --> (Recipe) --> (Tasty dish)!
+
+
+algorithm works as same of the above secuence.[(input) --> (algorithm) --> (output).  ]
+algorithm is language independent.we can implement it with any type of language. */
+
+
+//why algorithm?
+//As a developer you're going to come accross problems that you need to solve.learning algorithm translates to learning different techniques to effciently solve those problems
+//one problem can be solved in many ways using diffenrt algorithms.
+//every algorithm comes with its own tradeoffs when it comes to perfonmance.
+
+
+//algorithm analysis:
+//there are multiple ways to solve one problem.
+//so we sould go with the algorithm whice have the better performence..
+//the absolute running time of an algorithm can not be predicted ,since it depends ona number of factors.
+
+
+/* 1.programming language used to implement the algorithm.
+2.the computer the program runs on.
+3.other programs running at the same time.
+4.quality of the oparating system.  */
+
+//algorithm analysis contd:
+//we evaluate the performance of an algorithm in terms of its input size.
+//time complexity: amount of time taken by an algorithm to run, as a function of input size.
+//space complexity: amount of memory taken by an algorithm to run,as a function of input size.
+//by evaluating against the input size ,the analysis is not only machine independent but the comparison is also more appropiate.
