7b11e68c97c027f7b5e93e6a6622ef97e5f9403f
Emrul Hassan <hemrul494@gmail.com>
2022-10-06 00:10:26 +0600

    recursion is fun

--- algorithm/recurtion.js
+++ algorithm/recurtion.js
@@ -11,4 +11,13 @@
 
 //Analogy: if principal asked to two proffessor for getting student number.we can simplify it first!
 //principel=> (proffessor1 -> (teacher1(40) + teacher2(45))) + (proffessor2 -> (teacher3(23) + teacher4(60)))
-//firstly 
+//firstly the sum of two sum return to the proffessors;then the sum of two proffessors return it to the principel.
+
+
+//A few points about recursion.
+//Every recursive solution needs to have a base case --a condition to terminate the recursion.
+/*recursion might simpify solving a problem but it does not
+always translate to a faster solution . A recursive solution may be 
+for worse compared to an iterative solution */
+
+//recursion is a topic that is not the most straight forward to understand. do not give up if you straggle with the concept!
