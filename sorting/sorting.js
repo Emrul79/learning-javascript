278648027d97ffac9f4263ad07db66e350a6aff3
Emrul Hassan <hemrul494@gmail.com>
2022-10-08 23:58:54 +0600

    bubble sort

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -14,3 +14,13 @@
 //bubblesort(array)=> [-9,-2,4,5,6,8];
 
 
+//bubble sort idea:
+//compare adjacent elements in the array and swap the positions if they are not in the intened order;
+//repeat the instruction as you atep through each element in the array;
+//once you step through the whole array with no swap , the array is sorted;
+//[-6,3,4,1,2,6]
+//[-6,3,4,1,2,6]=>[-6,3,1,4,2,6]swap since 4>1;
+//[-6,3,1,4,2,6]=>[-6,3,1,2,4,6]swap since 4>2;
+//[-6,3,1,2,4,6]=>[-6,1,3,2,4,6]swap since 3>1;
+//[-6,1,3,2,4,6]=>[-6,1,2,3,4,6]swap since 3>2;
+//array is sorted;
