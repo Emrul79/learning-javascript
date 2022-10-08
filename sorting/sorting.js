9677fbf7ce66f22ff8989c9ca264dc4320e3987b
Emrul Hassan <hemrul494@gmail.com>
2022-10-08 23:47:57 +0600

    sort algorithm

---
+++ sorting/sorting.js
@@ -0,0 +1,16 @@
+// sorting algorithm;
+/*
+1.bubble sort;
+2.insertion sort;
+3.quik sort;
+4.merge sort;
+*/
+
+
+//Bubble sort;
+//problem : given an array of integers. sort the array;
+//sorting will either be ascending or decending;
+//const array = [-9,8,5,-2,4,6] ;
+//bubblesort(array)=> [-9,-2,4,5,6,8];
+
+
