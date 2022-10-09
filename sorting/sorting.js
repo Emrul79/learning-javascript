242cdf7233ebb643d34f4fe5c9ff19065e0cfc39
Emrul Hassan <hemrul494@gmail.com>
2022-10-09 20:56:01 +0600

    insertion solved

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -41,4 +41,39 @@ function bubbleSort(array) {
   return array;
 }
 
+
 //this bubble sort algorithm has two loop; so the time complexity of this algorithm is => O(n^2);
+
+
+
+//Insertion sort:
+
+
+// Virtually split the array into a sorted and an unsorted part.
+//Assume that the first element is already sorted and remaining elements are unsorted .
+//select an unsorted element and compare with all elements in the sorted part.
+//if the elements in the sorted part is smaller than the selected element,procced to the next element in the unsorted part.Else ,shift larger elements in the sorted part towards the right.
+
+//Insert the selected elements at the right Index.
+//repeat till all the unsorted elements are placed in the right order.
+
+
+//Insertion sort sproblem solve:
+//problem:implement the Insertion sort:
+
+//solution:
+
+function InsertionSort(array){
+  for(var i=1; i<array.length; i++){
+    let numberToInsert= array[i]; //[9,7,6,9,4,5]=>i=3; j=0;j=4;
+    let j= i-1;
+    while(j>=0 && array[j] > numberToInsert){
+      array[j+1] = array[j]//j=1===3; j=4;
+      j=j-1;
+    }
+    array[j+1] = numberToInsert
+  }
+  return array
+}
+
+console.log(InsertionSort([4,3,5,1,23,4,65,32,4,5,3,2,2,34,534]));
