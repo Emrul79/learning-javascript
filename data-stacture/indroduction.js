d0e9eeb4dad591f50b5d743a28ffb526b083f4e7
Emrul Hassan <hemrul494@gmail.com>
2022-10-15 02:22:54 +0600

    introductions

---
+++ data-stacture/indroduction.js
@@ -0,0 +1,24 @@
+//before we begin:
+
+//this course is a continution of the javascript algorithm series;
+//different proggramming languages have different data staucture . our choise of the language will be javascript;
+//it is possible to implement a data stucture in a few ways;
+//please do not memorise the implementation;
+
+
+//what is data structure?
+//a data structure is a way to store and organize data so that it can be used effciently;
+
+//a data structure is a collection of data values , the relationships among them ,and  the functions or operations that can be applied to that data;
+
+//why learn data structure?
+// almost every applicationwe build involves data that is modelled in a certain way to effciently manage that data ,you need data structure;
+//imagine walking into a library only to find out there is no affecient organization of the books and they are stored randomly;
+//data structure help us solve problem in a more efficient way, both in terms of time and memory;
+
+//learning about data structure also help you gain a more profound understanding of things you're already aware of.
+
+//for example:
+//DOM is contained a = Tree Data Structure;
+//Browser back and forword button= Stack data stracture;
+//OS jab scheduling = Queue data structure;
