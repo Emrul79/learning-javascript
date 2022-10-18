aff21de09049847be79a6e5fb9efc3969804c466
Emrul Hassan <hemrul494@gmail.com>
2022-10-18 23:46:57 +0600

    problem solving

--- practice/practice.js
+++ practice/practice.js
@@ -22,4 +22,42 @@ function duplycate(string) {
   return newarr.join(" ");
 }
 
-console.log(duplycate(s));
+// console.log(duplycate(s));
+
+
+
+
+
+//leetcode problem solving:
+
+//two sum:
+
+// Input: nums=[2,4,3,5,12,32], target=14;
+//output=[1,4];
+
+function twoSum(array,target){
+  for (var i=0; i<array.length; i++){
+    for(var j=1; j<array.length; j++){
+      if(array[i]+array[j]===target){
+        return [i,j]
+      }
+    }
+  }
+}
+// console.log(twoSum([12, 5, 75, 4, 65, 3, 21, 1, 2], 67));
+//time complexity of this algorithm is : O(n^2);
+//space complexity of this algorithm is : O(1);
+
+//we can do it with another way:
+function myTwoSum(array,target){
+  let memmory={};
+  for (var i=0; i<array.length; i++){
+    if(memmory[array[i]]===undefined){
+      memmory[target-array[i]]=i;
+    }else{
+     
+       console.log([i, memmory[array[i]]]);
+    }
+  }
+}
+myTwoSum([12, 5, 75, 4, 65, 3, 21, 1, 2], 67);
